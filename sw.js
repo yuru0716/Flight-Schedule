b1846b6e54e3d1f99083af43af58afb058f006c9
yuru0716 <105715252+yuru0716@users.noreply.github.com>
2026-02-12 13:27:03 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,14 @@
-const CACHE_NAME = 'flight-pwa-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const CACHE_NAME = 'task-airline-v7';
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+self.addEventListener('install', e => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
   );
+  self.clients.claim();
 });
