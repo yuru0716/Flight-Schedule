1f6c0ae02734852331fe7a2cb12b9ca8d6ccf688
yuru0716 <105715252+yuru0716@users.noreply.github.com>
2026-02-14 14:47:47 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -38,18 +38,3 @@ self.addEventListener('fetch', event => {
     })
   );
 });
-
-self.addEventListener("push", event => {
-    const data = event.data?.json() || {};
-
-    self.registration.showNotification(data.title || "TASK Airline", {
-        body: data.body || "チェックインの時間です",
-        icon: "/icon-192.png",
-        badge: "/icon-192.png"
-    });
-});
-
-self.addEventListener("notificationclick", event => {
-    event.notification.close();
-    event.waitUntil(clients.openWindow("/"));
-});
